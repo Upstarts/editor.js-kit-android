1e5c9884336dba572189461fa8585c48a9c8f484
Ruslan Aliev <RShAliev@sberbank.ru>
2022-05-30 00:49:51 +0300

    update dependencies

--- editorjskit/src/main/java/work/upstarts/editorjskit/environment/Extensions.kt
+++ editorjskit/src/main/java/work/upstarts/editorjskit/environment/Extensions.kt
@@ -42,10 +42,7 @@ fun ImageView.loadImage(url: String?, data: EJImageData) {
 }
 
 val Int.dp: Int
-    get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
-
-val Int.sp: Int
-    get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 fun Spannable.applyThemeForUrlSpans(theme: EJStyle?, context: Context) {
     for (u in getSpans(0, length, URLSpan::class.java)) {
